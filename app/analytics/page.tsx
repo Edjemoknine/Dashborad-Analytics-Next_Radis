0520d6fed77d3920eb42d0c90c88c7037cb1a1a9
moknine <mokninemoknine999@gmail.com>
2024-02-12 23:15:12 +0100

    last commit

--- app/analytics/page.tsx
+++ app/analytics/page.tsx
@@ -26,10 +26,49 @@ const Analytics = async () => {
         }, 0)
       );
     }, 0);
+
+  const topCountryisMap = new Map<string, number>();
+
+  for (let i = 0; i < pageviews.length; i++) {
+    const day = pageviews[i];
+    if (!day) continue;
+
+    for (let j = 0; j < day.events.length; j++) {
+      const event = day.events[j];
+      if (!event) continue;
+
+      const key = Object.keys(event)[0]!;
+      const value = Object.values(event)[0]!;
+      const parseKey = JSON.parse(key);
+
+      const country = parseKey?.country;
+      if (country) {
+        if (topCountryisMap.has(country)) {
+          const prev = topCountryisMap.get(country)!;
+          topCountryisMap.set(country, prev + value);
+        } else {
+          topCountryisMap.set(country, value);
+        }
+      }
+    }
+  }
+
+  const TopCountries = [...topCountryisMap.entries()]
+    .sort((a, b) => {
+      if (a[1] > b[1]) return -1;
+      else return 1;
+    })
+    .slice(0, 5);
+
   return (
     <div className="min-h-screen w-full py-12 flex justify-center items-center">
       <div className="relative w-full max-w-6xl mx-auto text-white">
-        <AnalyticsDashboard avgVisitorsPerDay={avgVisitorsPerDay} />
+        <AnalyticsDashboard
+          topCountries={TopCountries}
+          timeseriousviews={pageviews}
+          amtVistorssToday={amtVistorssToday}
+          avgVisitorsPerDay={avgVisitorsPerDay}
+        />
       </div>
     </div>
   );
--- app/components/AnalyticsDashboard.tsx
+++ app/components/AnalyticsDashboard.tsx
@@ -1,11 +1,21 @@
 "use client";
 
-import { Card } from "@tremor/react";
+import { Card, BarChart } from "@tremor/react";
 import React from "react";
+import { analytics } from "../utils/analytics";
+import { ReactCountryFlag } from "react-country-flag";
 interface AnalyticsDashbordProps {
   avgVisitorsPerDay: string;
+  amtVistorssToday: number;
+  timeseriousviews: Awaited<ReturnType<typeof analytics.retrieveDays>>;
+  topCountries: [string, number][];
 }
-const AnalyticsDashboard = ({ avgVisitorsPerDay }: AnalyticsDashbordProps) => {
+const AnalyticsDashboard = ({
+  avgVisitorsPerDay,
+  amtVistorssToday,
+  timeseriousviews,
+  topCountries,
+}: AnalyticsDashbordProps) => {
   return (
     <div className="flex flex-col gap-6">
       <div className=" grid grid-cols-1 md:grid-cols-2 w-full mx-auto gap-6">
@@ -17,7 +27,55 @@ const AnalyticsDashboard = ({ avgVisitorsPerDay }: AnalyticsDashbordProps) => {
             {avgVisitorsPerDay}
           </p>
         </Card>
+        <Card className="w-full mx-auto max-w-xs">
+          <p className="text-tremor-default text-dark-tremor-content">
+            visitors Today
+          </p>
+          <p className="text-3xl text-dark-tremor-content-strong font-semibold">
+            {amtVistorssToday}
+          </p>
+        </Card>
       </div>
+      <Card className="flex flex-col sm:grid grid-cols-4 gap-6">
+        <h2 className="w-full text-dark-tremor-content-strong text-center sm:text-left font-semibold text-xl">
+          This weeks top visitors
+        </h2>
+        <div className="col-span-3 flex items-center justify-between flex-wrap gap-8">
+          {topCountries.map((country, number) => {
+            return (
+              <div
+                className="flex items-center gap-3 text-dark-tremor-content-strong"
+                key={country}
+              >
+                <p className="hidden sm:block text-tremor-content">{country}</p>
+                <ReactCountryFlag
+                  svg
+                  countryCode={country}
+                  className="text-5xl sm:text-3xl"
+                />
+
+                <p className="text-tremor-content-strong ">{number}</p>
+              </div>
+            );
+          })}
+        </div>
+      </Card>
+      <Card className="">
+        {timeseriousviews ? (
+          <BarChart
+            showAnimation
+            data={timeseriousviews.map((day) => ({
+              name: day.date,
+              Visitors: day.events.reduce((acc, curr) => {
+                return acc + Object.values(curr)[0]!;
+              }, 0),
+            }))}
+            allowDecimals={false}
+            categories={["Visitors"]}
+            index="name"
+          ></BarChart>
+        ) : null}
+      </Card>
     </div>
   );
 };
